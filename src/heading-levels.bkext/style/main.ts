51e6253c3aeca380da0abe4d6c9861f332e1e7b8
Jesse Grosjean <jesse@hogbaysoftware.com>
2026-03-25 13:19:06 -0400

    fix for typescript 6

--- src/heading-levels.bkext/style/main.ts
+++ src/heading-levels.bkext/style/main.ts
@@ -5,7 +5,7 @@ let modifier = defineEditorStyleModifier('headinglevels', 'Heading Levels')
 modifier.layer('row-formatting', (row) => {
   for (let level = 1; level <= 6; level++) {
     row(`.heading level() = ${level}`, (context, row) => {
-      row.text.color = context.theme.colors.get(`heading${level}`)
+      row.text.color = context.theme.colors.get(`heading${level}`) ?? context.theme.colors.text
     })
   }
 })
