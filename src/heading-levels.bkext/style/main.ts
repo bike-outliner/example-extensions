9e9112d4ab1c310d183ec333f0829bd507f99a43
Zak Greant <zak@greant.com>
2026-03-27 13:21:39 +0100

    Add text scaling for heading levels in editor styles

--- src/heading-levels.bkext/style/main.ts
+++ src/heading-levels.bkext/style/main.ts
@@ -1,11 +1,13 @@
 import { Color, defineEditorStyleModifier } from 'bike/style'
 
 let modifier = defineEditorStyleModifier('headinglevels', 'Heading Levels')
+let size = [1.6, 1.4, 1.2, 1.1, 1, 0.9]
 
 modifier.layer('row-formatting', (row) => {
   for (let level = 1; level <= 6; level++) {
     row(`.heading level() = ${level}`, (context, row) => {
       row.text.color = context.theme.colors.get(`heading${level}`) ?? context.theme.colors.text
+      row.text.scale = size[level - 1]
     })
   }
 })
